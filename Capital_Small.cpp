f35f8613b6c9520c09440015d1651c7250af7966
Faizahamed-S <faizahamedshaik1200@gmail.com>
2024-03-06 00:06:11 -0800

    First Cpp Program

---
+++ Capital_Small.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cin >> c;
+    int ans;
+    if(c>=65 && c<=90)
+    ans=1;
+    else
+    ans=0;
+    cout << ans;
+
+    return 0;
+}
