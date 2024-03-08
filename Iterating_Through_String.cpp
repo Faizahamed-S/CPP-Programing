02bffde892be8086d26b73db2306c080a5b563cb
Faizahamed-S <faizahamedshaik1200@gmail.com>
2024-03-07 20:59:33 -0800

    Second Program

---
+++ Iterating_Through_String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+ string faiz;
+ cin>>faiz;
+ for(int i=0;i<faiz.length();i++)
+ {
+     cout<<faiz.at(i)<<endl;
+ }
+
+    return 0;
+}
