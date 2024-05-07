3acb0a41f6be270c6fbfbcfe576c5843933f087f
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2024-05-07 15:51:04 -0700

    Create Largest_Element_in_an_Array.cpp

---
+++ Step 3 - Problems on Arrays/Largest_Element_in_an_Array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int largestElement(vector<int> &arr, int n) {
+    sort(arr.begin(),arr.end());
+    return arr[arr.size()-1];
+}
+
+int main()
+{
+int n;
+cin>>n;
+vector <int> arr(n);
+for(int i=0;i<n;i++)
+{
+    int num;
+    cin>>num;
+   arr.push_back(num);
+}
+int ans=largestElement(arr,n);
+cout<<ans;
+}
